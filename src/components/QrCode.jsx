79b77a14a65243e352161bbdb035df75606fa53e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-09 22:38:18 +0300

    add form

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -2,10 +2,28 @@
 
 function QrCode() {
   return (
-      <div className="pt-5">
-          <h2 className="text-center">Qr Code Genius</h2>
+    <div className="pt-5">
+      <div className="row col-6 mx-auto">
+        <div className="card text-center">
+          <div className="card-header">QR Code Genius</div>
+          <div className="card-body">
+            <h5 className="card-title">Enter Text or URL</h5>
+            <p className="card-text">
+              <form action="">
+                <div className="form-group">
+                  <input className="form-control" type="text" />
+                </div>
+              </form>
+            </p>
+            <a href="#" className="btn btn-primary">
+              Generate QR Code
+            </a>
+          </div>
+          <div className="card-footer text-muted">2 days ago</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default QrCode
