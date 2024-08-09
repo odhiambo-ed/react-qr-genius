ea596f5cfbe531817be596e6abaf4f58ebde37d4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-09 22:21:41 +0300

    add style to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import QrCode from './components/QrCode'
 function App() {
 
   return (
-    <div className='container'>
+    <div className='container-fluid bg-light vh-100 vw-100'>
       <QrCode />
     </div>
   )
