36238fc0be10cf35f06d614906f9949cad607521
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-10 00:01:58 +0300

    add print and download

--- src/components/QrCodeGen.jsx
+++ src/components/QrCodeGen.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import QRCode from "react-qr-code";
+import { toPng } from "html-to-image";
+import download from "downloadjs";
 
 function QrCodeGen() {
   const [value, setValue] = useState("")
@@ -10,7 +12,25 @@ function QrCodeGen() {
     e.preventDefault();
     setQr(value)
     setValue("")
+
   }
+
+  const handleDownload = () => {
+    if (qrRef.current) {
+      toPng(qrRef.current)
+        .then((dataUrl) => {
+          download(dataUrl, "qr-code.png");
+        })
+        .catch((err) => {
+          console.error("Failed to download QR code", err);
+        });
+    }
+  };
+
+  const handlePrint = () => {
+    window.print();
+  };
+
   return (
     <div className="pt-5">
       <div className="row col-6 mx-auto">
@@ -41,7 +61,25 @@ function QrCodeGen() {
             </button>
           </div>
           <div className="card-footer">
-            {value && <QRCode size={256} value={qr} />}
+            {value && (
+              <>
+                <QRCode ref={qrRef} size={256} value={qr} />
+                <p>
+                  <button
+                    onClick={handleDownload}
+                    className="btn btn-secondary mt-3"
+                  >
+                    Download QR Code
+                  </button>
+                  <button
+                    onClick={handlePrint}
+                    className="btn btn-success mt-3 ml-2"
+                  >
+                    Print QR Code
+                  </button>
+                </p>
+              </>
+            )}
           </div>
         </div>
       </div>
